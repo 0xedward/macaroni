aaaa93ad21c1e1a7db35b97d4199c9784522e97d
Edward <14011954+0xedward@users.noreply.github.com>
2021-05-15 19:29:22 -0400

    Initial release
    * Add .gitignore for Node
    
    * Set up NPM
    
    * Create ESLint config
    
    * Initial commit

---
+++ src/background.js
@@ -0,0 +1,92 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener( function() {
+  chrome.contextMenus.create({
+    'id': 'base64Encode',
+    'title': 'Copy as base64',
+    'contexts': ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    'id': 'urlEncode',
+    'title': 'Copy as URL encoded string',
+    'contexts': ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    'id': 'jsonEscape',
+    'title': 'Copy as JSON escaped string',
+    'contexts': ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    'id': 'htmlEntityEncode',
+    'title': 'Copy as HTML Entity encoded string',
+    'contexts': ['selection'],
+  });
+
+  chrome.contextMenus.onClicked.addListener(function(info) {
+    if (typeof info != 'undefined') {
+      let resultString = '';
+      switch (info.menuItemId) {
+        case 'base64Encode':
+          resultString = base64Encode(info.selectionText);
+          break;
+        case 'urlEncode':
+          resultString = urlEncode(info.selectionText);
+          break;
+        case 'jsonEscape':
+          resultString = jsonEscape(info.selectionText);
+          break;
+        case 'htmlEntityEncode':
+          resultString = htmlEntityEncode(info.selectionText);
+          break;
+      }
+      if (resultString !== '') {
+        copyTextToClipboard(resultString);
+      }
+    }
+  });
+},
+);
+
+// https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension/18455088#18455088
+function copyTextToClipboard(text) {
+  const copyFrom = document.createElement('textarea');
+  copyFrom.textContent = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy', false, null);
+  document.body.removeChild(copyFrom);
+}
+
+function base64Encode(text) {
+  return btoa(text);
+}
+
+function urlEncode(text) {
+  const urlEncodedString = encodeURIComponent(text)
+      .replace(/'/g, '%27')
+      .replace(/"/g, '%22')
+      .replace(/\(/g, '%28')
+      .replace(/\)/g, '%29')
+      .replace(/\*/g, '%2A');
+  return urlEncodedString;
+}
+
+// https://gist.github.com/mathiasbynens/d6e10171d44a59bb5664617c64ff2763
+function jsonEscape(text) {
+  const json = JSON.stringify(text, null, 2);
+  const jsStringLiteral = JSON.stringify(json).slice(1, -1);
+  return jsStringLiteral;
+}
+
+function htmlEntityEncode(text) {
+  const htmlEntityEncodedString = text.replace(/"/g, '&quot;')
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&#039;')
+      .replace(/'/g, '&#039;');
+  return htmlEntityEncodedString;
+}
